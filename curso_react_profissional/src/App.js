ab963d29efa3c7076f3cdde3b2b14bff039ee382
adriel <adrielcarlos1010@gmail.com>
2019-01-24 01:04:00 -0200

    novo curso

---
+++ curso_react_profissional/src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+constructor(props){
+  super(props);
+  this.state = {
+    count: 0,
+    name: '',
+    lastName: ''
+  }
+}
+
+
+  contarClique(){
+    this.setState({
+      nome: this.state.nome
+    })
+  }
+
+  contarClique2(){
+    this.setState({
+      count: this.state.count +  1
+    })
+  }
+
+  mens(event){
+    this.setState({
+      lastName: event.target.value
+    })
+  }
+
+  onChangeEvent(key, valor){
+    this.setState({
+      [key]: valor.target.value
+    })
+  }
+
+
+
+ render() {
+    return (
+      <div className="App">
+
+        <p>Contador: {this.state.count}</p>
+        <button onClick={this.contarClique.bind(this)}>
+          Contar
+        </button>
+        <br />
+        <input type='text'  value={this.state.nome}  onChange={(e) => {
+            this.setState({
+              name: e.target.value
+            })
+        }}/>
+
+        <br />
+        <input type='text' value={this.state.lastName}  onChange={this.mens.bind(this)}/>
+
+          <p>{this.state.name} {this.state.lastName}</p>
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
